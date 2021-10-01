6c68bf045e66522427829c49121f5ec26da7d1b7
Vedant Dixit <75058610+vedantdixit@users.noreply.github.com>
2021-10-01 12:34:39 +0530

    Delete otpGenerator_logic_3.java

--- otpGenerator_logic_3.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class otpGenerator_logic_3 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in)
-        System.out.println("OTP Generator Logic 3");
-
-    }
-}
