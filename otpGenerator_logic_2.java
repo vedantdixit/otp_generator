6d05787f0f52a9a84fc67aad0a1c0b8cb0d82b23
Vedant Dixit <75058610+vedantdixit@users.noreply.github.com>
2021-10-01 12:34:12 +0530

    Update otpGenerator_logic_2.java

--- otpGenerator_logic_2.java
+++ otpGenerator_logic_2.java
@@ -3,7 +3,7 @@ public class otpGenerator_logic_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("OTP Generator logic 2");
-        int otp = (int)(Math.random() * ((99 - 11)+1)) + 11;
+        int otp = (int)(Math.random() * ((99 - 11)+1)) + 11; //here, it will give 4 Digits, you can change it according to your need ;)
         if (otp>=11)
             otp=otp*112;
         System.out.println("The OTP is: " + otp);
